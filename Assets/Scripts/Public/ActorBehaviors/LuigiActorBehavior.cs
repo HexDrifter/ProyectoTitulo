77e8e8759b90b1a9cb575ff5758fab64b2394939
HexDrifter <42625632+HexDrifter@users.noreply.github.com>
2024-05-29 18:40:33 -0400

    Improved Animations, background, tileset

---
+++ Assets/Scripts/Public/ActorBehaviors/LuigiActorBehavior.cs
@@ -0,0 +1,12 @@
+using ProyectoTitulo.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuigiActorBehavior : PlayerActorBehavior
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
